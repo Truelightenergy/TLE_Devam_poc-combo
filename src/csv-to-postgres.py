3987152a3192b77ebeb52faea753b155a6f2e0e2
Ronald Petty <ronald.petty@rx-m.com>
2022-12-19 21:37:55 -0800

    initial sqlalchemy hookup

--- src/csv-to-postgres.py
+++ src/csv-to-postgres.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import re
+import psycopg2
+import sqlalchemy as sa
 
 class ParseError(Exception):
     pass
@@ -25,8 +27,40 @@ def validate(f):
     return results
 
 def ingestion(f, r):
-    csv = pd.read_csv(f, header=4, nrows=1) # acts as context manager
-    print(csv)
+    # using file name instead of rows to infer things like control zone and strip
+    # this assumes the headers are like this:
+    # Control Area	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO
+    # ZoneID	28	29	30	31	32	33	34	35	36	37	38
+    # Zone	ZONE A	ZONE B	ZONE C	ZONE D	ZONE E	ZONE F	ZONE G	ZONE H	ZONE I	ZONE J	ZONE K
+    # Month	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE	FWD CURVE
+    # 12/1/22	$54.42 	$54.42 	$55.47 	$52.99 	$49.77 	$98.78 	$84.39 	$83.88 	$81.92 	$89.62 	$89.39 
+    # 1/1/23	$78.26 	$89.19 	$84.10 	$81.02 	$71.02 	$229.99 	$207.18 	$195.14 	$198.00 	$203.89 	$203.40 
+    # ...
+    # WARNING the provided CSV has many empty rows which are not skipped because they are empty strings
+    date_cols = ['Zone']
+    df = pd.read_csv(f, header=[2], skiprows=(3,3), dtype={
+        'Zone': str, # Month aka Zone due to bad header
+        # 'Zone A': pd.Float64Dtype, # ask guys on precision https://beepscore.com/website/2018/10/12/using-pandas-with-python-decimal.html
+        # 'Zone B': pd.Float64Dtype,
+        # 'Zone C': pd.Float64Dtype,
+        # 'Zone D': pd.Float64Dtype,
+        # 'Zone E': pd.Float64Dtype,
+        # 'Zone F': pd.Float64Dtype,
+        # 'Zone G': pd.Float64Dtype,
+        # 'Zone H': pd.Float64Dtype,
+        # 'Zone I': pd.Float64Dtype,
+        # 'Zone J': pd.Float64Dtype,
+        # 'Zone K': pd.Float64Dtype
+    }, parse_dates=date_cols).dropna() # acts as context manager
+    df[df.columns[1:]] = df[df.columns[1:]].replace('[\$,]', '', regex=True).astype(float) # see warning on Float64Dtype, this removes money and converts to float
+    print(df)
+    print(len(df.index))
+    print(df.dtypes)
+
+    engine = sa.create_engine("postgresql://docker:docker@localhost:5432/trueprice")
+    r = pd.read_sql(sa.text("SELECT * FROM trueprice.data"), engine)
+    print(r)
+
     None
 
 def validate_api(f, r):
