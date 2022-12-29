89a6df2b5863a500c1352aa41704483692684234
Ronald Petty <ronald.petty@rx-m.com>
2022-12-29 18:20:56 -0500

    prepped for scd-2

--- buildContext/src/ingestor.py
+++ buildContext/src/ingestor.py
@@ -6,25 +6,42 @@ import os
 class ParseError(Exception):
     pass
 
-def storage(f, r):
+def storage(m):
     pass
 
 def validate(f):
-    # ForwardCurve_NYISO_2X16_20221209.csv
+    # e.g. ForwardCurve_NYISO_2X16_20221209_010101.csv
     # curveType_iso_strip_curveDate.csv
-    file_name_components_pattern = re.compile("(.+)_(.+)_(.+)_(.+).csv$") # len(4)
+    # curveType_iso_strip_curveDate_curveTime.csv
+    # curveType_iso_strip_curveDate_cob.csv
+    file_name_components_pattern = re.compile("(.+?)_(.+?)_(.+?)_(.+?)(_.+)?.csv$") # len(4)
     
     print(f"Checking file name {f}")        
-    # "ForwardCurve_NYISO_2X16_20221209.csv"
     matched = file_name_components = file_name_components_pattern.search(f)
     if matched == None:
-        return ParseError(f"failed to parse {f}")
+        return ParseError(f"failed to parse {f} - regex")
     
-    results = matched.group(1,2,3,4) 
-    if len(results) != 4:
-        return ParseError(f"failed to parse {f}")
+    results = matched.groups() # 5 might be None if original name
+    if len(results) != 5:
+        return ParseError(f"failed to parse {f} - component count")
 
-    return results
+    (curveType, controlArea, strip, curveDate, issue) = results    
+    curveType = os.path.basename(curveType).replace("Curve","")
+
+    # add default value here for time, maybe not worth it, need to see how it flows
+    timeComponent = None
+    if issue is None:    
+        timeComponent = "000000"
+    else:
+        timeStamp = issue[1:] # drop leading underscore; else fix regex above
+        if timeStamp == "cob":
+            timeComponent = "115959" # convert to last possible second so we can sort properly
+        elif int(timeStamp):
+            timeComponent = timeStamp
+        else:
+            return ParseError(f"failed to parse {f} - time component")
+
+    return TLE_Meta(f, curveType, controlArea, strip, curveDate+timeComponent)
 
 def scd_2_panda():
     database = os.environ["DATABASE"]
@@ -44,7 +61,7 @@ def scd_2_panda():
     r3 = r2.join(r1, on="id", )
     print(r3)
 
-def ingestion(f, r):
+def ingestion(m):
     # using file name instead of rows to infer things like control zone and strip
     # this assumes the headers are like this:
     # Control Area	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO	NYISO
@@ -56,18 +73,10 @@ def ingestion(f, r):
     # ...
     # WARNING the provided CSV has many empty rows which are not skipped because they are empty strings
     date_cols = ['Zone']
-    df = pd.read_csv(f, header=[2], skiprows=(3,3), dtype={
+    df = pd.read_csv(m.fileName, header=[2], skiprows=(3,3), dtype={
         'Zone': str, # Month aka Zone due to bad header
         # 'Zone A': pd.Float64Dtype, # ask guys on precision https://beepscore.com/website/2018/10/12/using-pandas-with-python-decimal.html
-        # 'Zone B': pd.Float64Dtype,
-        # 'Zone C': pd.Float64Dtype,
-        # 'Zone D': pd.Float64Dtype,
-        # 'Zone E': pd.Float64Dtype,
-        # 'Zone F': pd.Float64Dtype,
-        # 'Zone G': pd.Float64Dtype,
-        # 'Zone H': pd.Float64Dtype,
-        # 'Zone I': pd.Float64Dtype,
-        # 'Zone J': pd.Float64Dtype,
+# ...
         # 'Zone K': pd.Float64Dtype
     }, parse_dates=date_cols).dropna() # acts as context manager
     df[df.columns[1:]] = df[df.columns[1:]].replace('[\$,]', '', regex=True).astype(float) # see warning on Float64Dtype, this removes money and converts to float
@@ -75,64 +84,115 @@ def ingestion(f, r):
 #    print(len(df.index))
 #    print(df.dtypes)
 
-    #create table test_data ( -- history table differs
-    # id serial PRIMARY KEY, 
-    # zone_a_amount numeric(12,8), 
-    # zone_b_amount numeric(12,8),
-    # zone_c_amount numeric(12,8),
-    # zone_d_amount numeric(12,8), 
-    # zone_e_amount numeric(12,8),     
-    # zone_f_amount numeric(12,8), 
-    # zone_g_amount numeric(12,8),
-    # zone_h_amount numeric(12,8),
-    # zone_i_amount numeric(12,8), 
-    # zone_j_amount numeric(12,8),     
-    # zone_k_amount numeric(12,8),    
-    # curveStart TIMESTAMP
-    # );
+    """
+    \c trueprice
+    CREATE DATABASE trueprice;
 
-    database = os.environ["DATABASE"]
-    pgpassword = os.environ["PGPASSWORD"]
-    pguser = os.environ["PGUSER"]
+        create table nyiso_forwardcurve ( -- history table differs
+        id serial PRIMARY KEY,
+        strip varchar(4), -- 7X8, 2X16, 7X24, etc. maybe enum one day
+        curveStart TIMESTAMP, -- per file name
+        zone_a_amount numeric(12,8), -- varies by iso, unclear best approach, single table or per iso table
+        zone_b_amount numeric(12,8),
+        zone_c_amount numeric(12,8),
+        zone_d_amount numeric(12,8), 
+        zone_e_amount numeric(12,8),     
+        zone_f_amount numeric(12,8), 
+        zone_g_amount numeric(12,8),
+        zone_h_amount numeric(12,8),
+        zone_i_amount numeric(12,8), 
+        zone_j_amount numeric(12,8),     
+        zone_k_amount numeric(12,8)
+        );
+
+        -- old data
+        -- union old and new data for all data (versus is_current column)
+        create table nyiso_forwardcurve_history ( -- current table differs
+        id serial, -- not primary, this is fk to nyiso_forwardcurve
+        strip varchar(4), -- 7X8, 2X16, 7X24, etc. maybe enum one day
+        curveStart TIMESTAMP, -- per original file name
+        curveEnd TIMESTAMP, -- per new file name
+        zone_a_amount numeric(12,8), -- varies by iso, unclear best approach, single table or per iso table
+        zone_b_amount numeric(12,8),
+        zone_c_amount numeric(12,8),
+        zone_d_amount numeric(12,8), 
+        zone_e_amount numeric(12,8),     
+        zone_f_amount numeric(12,8), 
+        zone_g_amount numeric(12,8),
+        zone_h_amount numeric(12,8),
+        zone_i_amount numeric(12,8), 
+        zone_j_amount numeric(12,8),     
+        zone_k_amount numeric(12,8)
+        );
+    """
+
+    # 
+    # if data in current -> backup
+    # if not in current -> insert current
+    #
+
+    #
+    # # what is "current data"
+    # curve & iso & strip & date (not price as it changes)
+    #
+
+    database = os.environ["DATABASE"] if "DATABASE" in os.environ else "postgres"
+    pgpassword = os.environ["PGPASSWORD"] if "PGPASSWORD" in os.environ else "postgres"
+    pguser = os.environ["PGUSER"] if "PGUSER" in os.environ else "postgres"
+
     engine = sa.create_engine(f"postgresql://{pguser}:{pgpassword}@{database}:5432/trueprice",
     #connect_args={'options': '-csearch_path=trueprice'}
     )
 
-    r = df.to_sql('data', con = engine, if_exists = 'replace', chunksize=1000, schema="trueprice")
+    check_query = f"select exists(select 1 from nyiso_forwardcurve where curveStart={m.curveDate} and strip={m.strip})"
 
-    if r is None:
-        print("failed to insert")
-        return
+    #r = df.to_sql('data', con = engine, if_exists = 'replace', chunksize=1000, schema="trueprice")
 
-    r = pd.read_sql(sa.text("SELECT * FROM trueprice.data"), engine)
-    print(r)
+    #if r is None:
+    #    print("failed to insert")
+    #    return
+
+    #r = pd.read_sql(sa.text("SELECT * FROM trueprice.data"), engine)
+    #print(r)
 
     None
 
-def validate_api(f, r):
+def validate_api(m):
     None
+
+class TLE_Meta:
+    def __init__(self, fileName, curveType, controlArea, strip, curveDate):
+        self.fileName = fileName
+        self.curveType = curveType
+        self.controlArea = controlArea
+        self.strip = strip
+        self.curveDate = curveDate
     
 def process(files, steps):
-    components = None
+    meta = None
 
     # validate
+    valid = []
     for f in files:
-        components = steps["v"](f)
-        if components is None or isinstance(components, ParseError):
-            return components # all files must pass, in case systematic errors
+        meta = steps["v"](f)
+        if meta is None or isinstance(meta, ParseError):
+            return meta # all files must pass, in case systematic errors
+        else:
+            valid.append(meta)
 
-    # store
-    for f in files:
-        result = steps["s"](f, components) # store before we place in db
+    # storage
+    # add sha later
+    for m in valid:
+        result = steps["s"](m) # store before we place in db
         if result is not None :
             return result
 
     # insert db / api check each as we go (need to find way to redo/short-circuit/single file, etc.)
-    for f in files:    
-        result = steps["i"](f, components) # insert/update db
+    for m in valid:
+        result = steps["i"](m) # insert/update db
         if result is not None:
             return result
-        result = steps["va"](f, components) # validate data in db via api
+        result = steps["va"](m) # validate data made it to db via api
         if result is not None:
             return result
 
@@ -142,9 +202,12 @@ if __name__ == "__main__":
     print("Starting")
 
     files = [
-        "./buildContext/data/ForwardCurve_NYISO_2X16_20221209.csv",
-        "./buildContext/data/ForwardCurve_NYISO_5X16_20221209.csv",
-        "./buildContext/data/ForwardCurve_NYISO_7X8_20221209.csv"
+        "./buildContext/data/ForwardCurve_NYISO_2X16_20221209.csv", # strip 1 assuming no HHMMSS is 000000 (midnight)
+        "./buildContext/data/ForwardCurve_NYISO_5X16_20221209.csv", # strip 2
+        "./buildContext/data/ForwardCurve_NYISO_7X8_20221209.csv", # strip 3
+        "./buildContext/data/ForwardCurve_NYISO_7X8_20221209_121212.csv", # strip 3 interday HHMMSS
+        "./buildContext/data/ForwardCurve_NYISO_7X8_20221209_cob.csv", # strip 3 cob
+        "./buildContext/data/ForwardCurve_NYISO_7X8_20221209_121212.csv", # strip 3 interday (should not do it)
     ]
 
     # v == validate files names/shape
