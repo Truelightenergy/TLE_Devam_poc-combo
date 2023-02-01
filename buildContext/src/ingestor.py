d12430223690cac68e3b5ed85a0715c6f094cb70
Ronald Petty <ronald.petty@rx-m.com>
2023-01-31 20:50:38 -0800

    draft anciallary data (not detail) -- new and update (single test) worked via URL

--- buildContext/src/ingestor.py
+++ buildContext/src/ingestor.py
@@ -53,47 +53,254 @@ def validate(f):
     timestamp = datetime.datetime.strptime(curveDate+timeComponent, "%Y%m%d%H%M%S")
     return TLE_Meta(f, curveType, controlArea, strip, timestamp)
 
-def scd_2_panda():
-    database = os.environ["DATABASE"]
-    pgpassword = os.environ["PGPASSWORD"]
-    pguser = os.environ["PGUSER"]
-    engine = sa.create_engine(f"postgresql://{pguser}:{pgpassword}@{database}:5432/trueprice",
-    #connect_args={'options': '-csearch_path=trueprice'}
-    )
+# def scd_2_panda():
+#     database = os.environ["DATABASE"]
+#     pgpassword = os.environ["PGPASSWORD"]
+#     pguser = os.environ["PGUSER"]
+#     engine = sa.create_engine(f"postgresql://{pguser}:{pgpassword}@{database}:5432/trueprice",
+#     #connect_args={'options': '-csearch_path=trueprice'}
+#     )
 
-    # new_data = receive new data
-    # if new_data not in current_db -> insert new to current_db (if fails???) // save data to local disk before next step?
-    # else new_data in current_db -> copy existing_data to history_db then update existing_data with new_data (if fails???) // save data to local disk before next step?
+#     # new_data = receive new data
+#     # if new_data not in current_db -> insert new to current_db (if fails???) // save data to local disk before next step?
+#     # else new_data in current_db -> copy existing_data to history_db then update existing_data with new_data (if fails???) // save data to local disk before next step?
 
-    r1 = pd.read_sql(sa.text("SELECT * FROM test_data"), engine) # current
-    r2 = pd.read_sql(sa.text("SELECT * FROM test_data_history"), engine) # history
+#     r1 = pd.read_sql(sa.text("SELECT * FROM test_data"), engine) # current
+#     r2 = pd.read_sql(sa.text("SELECT * FROM test_data_history"), engine) # history
     
-    r3 = r2.join(r1, on="id", )
-    print(r3)
+#     r3 = r2.join(r1, on="id", )
+#     print(r3)
 
-# for potential refactor
-ca = {
-    "nyiso": {
-        "table": "trueprice.nyiso_forwardcurve",
-        "zones": []
-    },
-    "miso": {
-        "table": "trueprice.miso_forwardcurve",
-        "zones": []
-    },
-    "pjm": {
-        "table": "trueprice.pjm_forwardcurve",
-        "zones": []
-    },
-    "isone": {
-        "table": "trueprice.isone_forwardcurve",
-        "zones": []
-    },
-    "ercot": {
-        "table": "trueprice.ercot_forwardcurve",
-        "zones": ["north_amount","houston_amount","south_amount","west_amount"],
-    }
-}
+# # for potential refactor
+# ca = {
+#     "nyiso": {
+#         "table": "trueprice.nyiso_forwardcurve",
+#         "zones": []
+#     },
+#     "miso": {
+#         "table": "trueprice.miso_forwardcurve",
+#         "zones": []
+#     },
+#     "pjm": {
+#         "table": "trueprice.pjm_forwardcurve",
+#         "zones": []
+#     },
+#     "isone": {
+#         "table": "trueprice.isone_forwardcurve",
+#         "zones": []
+#     },
+#     "ercot": {
+#         "table": "trueprice.ercot_forwardcurve",
+#         "zones": ["north_amount","houston_amount","south_amount","west_amount"],
+#     }
+# }
+
+def ad_ingestion(m):
+    df = pd.read_csv(m.fileName, parse_dates=['Month'])
+    df.rename(inplace=True, columns={
+        'Zone ID': 'zone_id',
+        'Load Zone': 'load_zone',
+        'Month': 'month',
+        'RTLO ($/MWh)': 'rtlo_mwh',
+        'DALO ($/MWh)': 'dalo_mwh',
+        'Deviations ($/MWh)': 'deviations_mwh',
+        'NCP ($/MW)': 'ncp_mw'
+    })
+
+    df.drop(columns=["zone_id"], inplace=True)
+
+    df.insert(0, 'strip', m.strip) # stored as object, don't freak on dtypes
+    df.insert(0, 'curvestart', m.curveStart) # date on file, not the internal zone/month column
+    
+    print(df)
+    print(len(df.index))
+    print(df.dtypes)
+
+    database = os.environ["DATABASE"] if "DATABASE" in os.environ else "localhost"
+    pgpassword = os.environ["PGPASSWORD"] if "PGPASSWORD" in os.environ else "postgres"
+    pguser = os.environ["PGUSER"] if "PGUSER" in os.environ else "postgres"
+
+    engine = sa.create_engine(f"postgresql://{pguser}:{pgpassword}@{database}:5432/trueprice")
+
+    sod = m.curveStart.strftime('%Y-%m-%d') # drop time, since any update should be new
+    now = m.curveStart
+    eod = (m.curveStart + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
+    check_query = f"""
+-- if nothing found, new data, insert it, or do one of these
+select exists(select 1 from trueprice.{m.controlArea}_ancillarydata where curvestart='{now}'and strip='{m.strip}') -- ignore, db == file based on timestamp
+UNION ALL
+select exists(select 1 from trueprice.{m.controlArea}_ancillarydata where curvestart>='{sod}' and curvestart<'{now}' and strip='{m.strip}') -- update, db is older
+UNION ALL
+select exists(select 1 from trueprice.{m.controlArea}_ancillarydata where curvestart>'{now}' and curvestart<'{eod}' and strip='{m.strip}') -- ignore, db is newer
+"""
+    r = pd.read_sql(check_query, engine)
+    same, old_exists, new_exists = r.exists[0], r.exists[1], r.exists[2]
+
+    if same:
+        print("Data already exists based on timestamp and strip")
+        return
+    elif not same and not new_exists and not old_exists:
+        r = df.to_sql(f"{m.controlArea}_ancillarydata", con = engine, if_exists = 'append', chunksize=1000, schema="trueprice", index=False)
+        if r is None:
+            print("Failed to insert")
+            return # add error
+    elif old_exists:
+        tmp_table_name = f"{m.controlArea}_ancillarydata_{m.snake_timestamp()}" # temp table to hold new csv data so we can work in SQL
+        r = df.to_sql(f'{tmp_table_name}', con = engine, if_exists = 'replace', chunksize=1000, schema="trueprice", index=False)
+        if r is None:
+            return SQLError(f"failed to create {tmp_table_name}")
+
+        with engine.connect() as con:
+            startOfCurveStart = m.curveStart.strftime('%Y-%m-%d')
+            curveend = m.curveStart # the new data ends the old data
+            backup_query = ''''''
+            if m.controlArea == "isone":
+                backup_query = f'''
+with current as (
+    -- get the current rows in the database, all of them, not just things that will change
+    select id, strip, curvestart, load_zone, rtlo_mwh, dalo_mwh, deviations_mwh, ncp_mw from trueprice.{m.controlArea}_ancillarydata where curvestart>='{sod}' and curvestart<='{eod}' and strip='{m.strip}'
+),
+backup as (
+    -- take current rows and insert into database but with a new "curveend" timestamp
+    insert into trueprice.{m.controlArea}_ancillarydata_history (id, strip, curvestart, curveend, load_zone, rtlo_mwh, dalo_mwh, deviations_mwh, ncp_mw)
+    select id, strip, curvestart, '{curveend}' as curveend, load_zone, rtlo_mwh, dalo_mwh, deviations_mwh, ncp_mw
+    from current
+),
+single as (
+    select curvestart from current limit 1
+)
+-- update the existing "current" with the new "csv"
+update trueprice.{m.controlArea}_ancillarydata set
+curvestart = newdata.curveStart, -- this reflects the intra update, should only be the time not the date
+load_zone = newdata.load_zone, -- mindless update all cols, we don't know which ones updated so try them all
+rtlo_mwh = newdata.rtlo_mwh,
+dalo_mwh = newdata.dalo_mwh,
+deviations_mwh = newdata.deviations_mwh,
+ncp_mw = newdata.ncp_mw
+from 
+    trueprice.{tmp_table_name} as newdata -- our csv data
+where 
+    trueprice.{m.controlArea}_ancillarydata.strip = newdata.strip 
+    and trueprice.{m.controlArea}_ancillarydata.month = newdata.month 
+    and trueprice.{m.controlArea}_ancillarydata.curvestart=(select curvestart from single)
+'''        
+            #elif m.controlArea == "nyiso":
+            # etc.
+            else:
+                print("Unknown update,abort")
+                return
+            print(backup_query)
+            r = con.execute(backup_query)            
+            print(r)
+            con.execute(f"drop table trueprice.{tmp_table_name}")
+    elif new_exists:
+        print("Newer data in database, abort")
+        return # needs error
+    else:
+        print("Ingestion logic error, we should not be here")
+        return # needs error
+
+    return
+
+def add_ingestion(m):
+    pass
+
+# REC Update Tool.csv --> REC_Update_Tool_20221231_135045.csv
+# waiting on jon
+def rec_ingestion(m):
+        # WARNING the provided CSV has many empty rows which are not skipped because they are empty strings
+    date_cols = ['Zone']
+    df = pd.read_csv(m.fileName, header=[2], skiprows=(3,3), dtype={
+        'Zone': str, # Bad CSV header (should be curveStart or Month)
+        # 'Zone A': pd.Float64Dtype, # ask guys on precision https://beepscore.com/website/2018/10/12/using-pandas-with-python-decimal.html
+# ...
+        # 'Zone K': pd.Float64Dtype
+    }, parse_dates=date_cols).dropna() # acts as context manager
+    df[df.columns[1:]] = df[df.columns[1:]].replace('[\$,]', '', regex=True).astype(float) # see warning on Float64Dtype, this removes money and converts to float
+
+    if m.controlArea == "nyiso":
+        df.rename(inplace=True, columns={
+            'Zone': 'month',
+            'ZONE A': 'zone_a_amount',
+            'ZONE B': 'zone_b_amount',
+            'ZONE C': 'zone_c_amount',
+            'ZONE D': 'zone_d_amount',
+            'ZONE E': 'zone_e_amount',
+            'ZONE F': 'zone_f_amount',
+            'ZONE G': 'zone_g_amount',
+            'ZONE H': 'zone_h_amount',
+            'ZONE I': 'zone_i_amount',
+            'ZONE J': 'zone_j_amount',
+            'ZONE K': 'zone_k_amount'
+            })
+    elif m.controlArea == "miso":
+        df.rename(inplace=True, columns={
+            'Zone': 'month',
+            'AMILCIPS': 'amilcips_amount',
+            'AMILCILCO': 'amilcilco_amount',
+            'AMILIP': 'amilip_amount',
+            'INDY HUB': 'indy_amount'
+        })
+    elif m.controlArea == "ercot":
+        df.rename(inplace=True, columns={
+            'Zone': 'month',  
+            'NORTH ZONE':'north_amount',
+            'HOUSTON ZONE':'houston_amount',
+            'SOUTH ZONE':'south_amount',
+            'WEST ZONE':'west_amount'
+        })
+    elif m.controlArea == "pjm":
+          df.rename(inplace=True, columns={
+            'Zone': 'month',
+            'AECO':'aeco_amount',
+            'AEP':'aep_amount',
+            'APS':'aps_amount', 
+            'ATSI':'atsi_amount', 
+            'BGE':'bge_amount', 
+            'COMED':'comed_amount', 
+            'DAY':'day_amount', 
+            'DEOK':'deok_amount', 
+            'DOM':'dom_amount', 
+            'DPL':'dpl_amount', 
+            'DUQ':'duq_amount', 
+            'JCPL':'jcpl_amount', 
+            'METED':'meted_amount', 
+            'PECO':'peco_amount', 
+            'PENELEC':'penelec_amount', 
+            'PEPCO':'pepco_amount', 
+            'PPL':'ppl_amount', 
+            'PSEG':'pseg_amount', 
+            'RECO':'reco_amount', 
+            'WEST HUB':'west_amount', 
+            'AD HUB':'ad_amount', 
+            'NI HUB':'ni_amount', 
+            'EAST HUB':'east_amount'
+          })
+    elif m.controlArea == "isone":
+        #Zone,MAINE,NEWHAMPSHIRE,VERMONT,CONNECTICUT,RHODEISLAND,SEMASS,WCMASS,NEMASSBOST,MASS HUB
+        df.rename(inplace=True, columns={
+            'Zone': 'month',
+            'MAINE':'maine_amount',
+            'NEWHAMPSHIRE': 'newhampshire_amount',
+            'VERMONT':'vermont_amount',
+            'CONNECTICUT':'connecticut_amount', 
+            'RHODEISLAND':'rhodeisland_amount', 
+            'SEMASS':'semass_amount', 
+            'WCMASS':'wcmass_amount', 
+            'NEMASSBOST':'nemassbost_amount', 
+            'MASS HUB':'mass_amount'
+        })
+    else:
+        return # error
+
+    df.insert(0, 'strip', m.strip) # stored as object, don't freak on dtypes
+    df.insert(0, 'curvestart', m.curveStart) # date on file, not the internal zone/month column
+    
+    print(df)
+    print(len(df.index))
+    print(df.dtypes)
+
 
 def ingestion(m):
     # WARNING the provided CSV has many empty rows which are not skipped because they are empty strings
@@ -186,7 +393,7 @@ def ingestion(m):
     
     print(df)
     print(len(df.index))
-    print(df.dtypes)    
+    print(df.dtypes)  
 
     # 
     # if data in current -> backup
@@ -496,7 +703,17 @@ def call_ingestor(file):
     import sys
     print(f"Processing {file}", file=sys.stderr) # flask capture stdout so need to use stderr for now, fix later
     files = [file]
-    result = process(files, {"v":validate, "s":storage, "i":ingestion, "va": validate_api})
+    result = None
+    if re.search("forward", file, re.IGNORECASE):
+        result = process(files, {"v":validate, "s":storage, "i":ingestion, "va": validate_api})
+    elif re.search("ancillarydata_details", file, re.IGNORECASE):
+        pass # todo
+    elif re.search("ancillarydata", file, re.IGNORECASE):
+        result = process(files, {"v":validate, "s":storage, "i":ad_ingestion, "va": validate_api})
+    else:
+        print("Shouldn't be here")
+        return
+
     if result is not None:
         print(f"Ingestion Failed: {result}")
     else:
