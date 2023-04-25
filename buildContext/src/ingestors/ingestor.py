917fb850e782792423c02139c6308c9ab986e639
MAliHaider-1444 <ali.haider@techliance.com>
2023-04-25 14:30:20 +0500

    ingestion fix

--- buildContext/src/ingestors/ingestor.py
+++ buildContext/src/ingestors/ingestor.py
@@ -116,6 +116,7 @@ class Ingestion:
         :param object_name: S3 object name. If not specified then file_name is used
         :return: True if file was uploaded, else False
         """
+        # return 
 
         # If S3 object_name was not specified, use file_name
         if object_name is None:
