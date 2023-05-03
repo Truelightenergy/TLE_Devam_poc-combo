8d1f37178b25c227a4aaa715597e1cb16977d7eb
Ronald Petty <ronald.petty@rx-m.com>
2023-05-03 16:34:37 -0700

    add "all" strip, removed invalid strips, cleaned up related query

--- buildContext/src/extractors/energy.py
+++ buildContext/src/extractors/energy.py
@@ -35,12 +35,12 @@ class Energy:
 
             if control_area == "nyiso" or control_area == "miso" or control_area == "ercot" or control_area == "pjm" or control_area == "isone":
                 data_frame = None
-                psql_query = f"select * from trueprice.{control_area}_energy where LOWER(strip) = '{strip.lower()}' and month::date >= '{start_date}' and month::date <= '{end_date}';"
+                psql_query = f"select * from trueprice.{control_area}_energy where (strip = '{strip.lower()}' or '{strip.lower()}' = 'all' ) and month::date >= '{start_date}' and month::date <= '{end_date}';"
                 print(psql_query)
                 data_frame = pd.read_sql_query(sql=psql_query, con=self.engine.connect())
                 return data_frame, "success"  
             else:
-                return None, "Unable to Fetch Results"
+                return None, f"Unable to fetch results, invalid control area: {control_area}"
 
         except:
             import traceback, sys
