ff4620bf623b12be07fdda8b1b5f5e41e5e5ec7a
MAliHaider-1444 <ali.haider@techliance.com>
2023-12-06 15:55:00 +0500

    notifier v2 changes

--- buildContext/src/cron_setup_notification.py
+++ buildContext/src/cron_setup_notification.py
@@ -8,6 +8,6 @@ if __name__=="__main__":
     util = Util()
     util.setup_notifications()
    
-    scheduler = BlockingScheduler({'apscheduler.job_defaults.max_instances': 2})
-    scheduler.add_job(util.setup_notifications, 'interval', minutes=5)
-    scheduler.start()
+    # scheduler = BlockingScheduler({'apscheduler.job_defaults.max_instances': 2})
+    # scheduler.add_job(util.setup_notifications, 'interval', minutes=5)
+    # scheduler.start()
