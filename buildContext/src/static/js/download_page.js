906b6495ffc2ebe84b08810316bc1fe53301636a
Atif <myresponsebox@live.com>
2024-04-24 17:02:36 +0500

    Minor Refactor

--- buildContext/src/static/js/download_page.js
+++ buildContext/src/static/js/download_page.js
@@ -26,9 +26,9 @@ function calculateDates(inputDate) {
     };
 }
 
-function data_loader(){
+function data_loader() {
     // Define the change event handler for curve type dropdown
-    $('#curve_type').change(function() {
+    $('#curve_type').change(function () {
         var selectedcurve = $(this).val();
         $.ajax({
             url: '/get_options',
@@ -36,11 +36,11 @@ function data_loader(){
             contentType: 'application/json',
             data: JSON.stringify({ 'curve': selectedcurve }),
             headers: {
-                'Authorization': "Bearer "+token
+                'Authorization': "Bearer " + token
             },
-            success: function(response) {
+            success: function (response) {
                 $('#iso').empty();
-                $.each(response, function(index, value) {
+                $.each(response, function (index, value) {
                     var html = '<option value="' + value.toLowerCase() + '">' + value + '</option>';
                     $('#iso').append(html);
                 });
@@ -56,18 +56,17 @@ function data_loader(){
             contentType: 'application/json',
             data: JSON.stringify({ 'curve': selectedcurve }),
             headers: {
-                'Authorization': "Bearer "+token
+                'Authorization': "Bearer " + token
             },
-            success: function(response) {
+            success: function (response) {
                 $('#strip').empty();
-                $.each(response, function(index, value) {
-                   
-                        var html = '<option selected value="' + "strip_"+value.toLowerCase() + '">' + value + '</option>';
+                $.each(response, function (index, value) {
+                    var html = `<option ${value === '7x24' ? 'selected' : ''} value="strip_${value.toLowerCase()}">${value}</option>`;
                     $('#strip').append(html);
-                    
+
                 });
                 $('#strip').selectpicker('refresh');
-                
+
             }
         });
     });
@@ -79,32 +78,32 @@ function data_loader(){
 
 
 function available(date) {
-    
+
     var sdate = moment(date).format('YYYY-MM-DD');
     if ($.inArray(sdate, availableDates) !== -1) {
-    return {
-        enabled: true,
-    }
+        return {
+            enabled: true,
+        }
     } else {
-    return {
-        enabled: false
-    }
+        return {
+            enabled: false
+        }
     }
 }
 
-function load_operating_day_calender(){    
+function load_operating_day_calender() {
     $("#operating_day").datepicker({
         todayHighlight: true,
         format: 'yyyy-mm-dd',
         multidate: false,
-        conatiner:'#odc',
+        conatiner: '#odc',
         beforeShowDay: available,
-      });
+    });
 }
 
-function load_operating_days(){
+function load_operating_days() {
 
-    $('#iso').change(function() {
+    $('#iso').change(function () {
 
         var selectediso = $(this).val();
         var selectedcurve = $('#curve_type').val();
@@ -114,17 +113,17 @@ function load_operating_days(){
             contentType: 'application/json',
             data: JSON.stringify({ 'curve': selectedcurve, 'iso': selectediso }),
             headers: {
-                'Authorization': "Bearer "+token
+                'Authorization': "Bearer " + token
             },
-            success: function(response) {
+            success: function (response) {
                 availableDates = response;
                 load_operating_day_calender();
-                if(response[0]!= undefined){
-                    $('#operating_day').datepicker("setDate", new Date(response[0]) );
+                if (response[0] != undefined) {
+                    $('#operating_day').datepicker("setDate", new Date(response[0]));
                 }
-                else{
-                    $('#operating_day').datepicker('refresh'); 
-                    $('#operating_day').datepicker("setDate", null);  
+                else {
+                    $('#operating_day').datepicker('refresh');
+                    $('#operating_day').datepicker("setDate", null);
                 }
             }
         });
@@ -134,39 +133,39 @@ function load_operating_days(){
     // $('#operating_day').trigger('change');
 }
 
-function filling_dates(selected_date){
+function filling_dates(selected_date) {
     response = calculateDates(selected_date);
     start = response["oneMonthLater"];
     end = response["sixtyOneMonthsLater"];
-    document.getElementById('start').value= start;
-    document.getElementById('end').value= end;
+    document.getElementById('start').value = start;
+    document.getElementById('end').value = end;
 }
-function date_updates(){
-    $('#operating_day').on("changeDate", function() {
+function date_updates() {
+    $('#operating_day').on("changeDate", function () {
         var selected_date = $(this).val();
-        if (selected_date!=null){
+        if (selected_date != null) {
             filling_dates(selected_date);
             cob_check(selected_date, $('#curve_type').val(), $('#iso').val());
         }
-        
+
 
     });
 }
 
-function cob_check(date, curve, iso){
+function cob_check(date, curve, iso) {
     $.ajax({
         url: '/cob_check',
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({ 'operating_day': date, 'curve': curve, 'iso':iso }),
+        data: JSON.stringify({ 'operating_day': date, 'curve': curve, 'iso': iso }),
         headers: {
-            'Authorization': "Bearer "+token
+            'Authorization': "Bearer " + token
         },
-        success: function(response) {
-            if (response){
-            $("#cob").prop("disabled", false); // disable
+        success: function (response) {
+            if (response) {
+                $("#cob").prop("disabled", false); // disable
             }
-            else{
+            else {
                 $("#cob").prop("disabled", true); // disable
             }
         }
@@ -175,9 +174,9 @@ function cob_check(date, curve, iso){
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     data_loader();
     load_operating_days();
     date_updates();
-    
+
 });
